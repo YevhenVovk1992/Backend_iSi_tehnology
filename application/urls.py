10198f509ccb8525608fd8a476627fd0a68ff1e9
YevhenVovk <vovk.valki@ukr.net>
2023-03-02 10:18:27 +0200

    Create ThreadList APIView to get list Thread with auth user
    add path 'application/thread/list/';
    make ThreadList, rewrite 'get_queryset' function;

--- application/urls.py
+++ application/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('thread/', ThreadDetail.as_view()),
+    path('thread/list/', ThreadList.as_view()),
     path('thread/<int:pk>/', ThreadDetail.as_view())
 ]
--- application/views.py
+++ application/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets, mixins, status, generics
 
 from application import models, serializer
@@ -20,3 +21,19 @@ class ThreadDetail(
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class ThreadList(generics.ListAPIView):
+    """
+
+    """
+    queryset = models.Thread.objects.all()
+    serializer_class = serializer.ThreadSerializer
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        queryset = models.Thread.objects.filter(
+            Q(participants__first_user=user_id) |
+            Q(participants__second_user=user_id)
+        ).all()
+        return queryset
