15927ca61754f0233ac13e84377750da0d7885af
YevhenVovk <vovk.valki@ukr.net>
2023-03-03 20:16:58 +0200

    Add comments for all models and serializer

--- application/models.py
+++ application/models.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 
 
 # Create your models here.
 class Thread(models.Model):
     """
-
+    The model that describes Thread object
     """
     participants = models.JSONField(null=False)
     created = models.DateTimeField(editable=False, auto_now_add=True)
@@ -15,10 +14,10 @@ class Thread(models.Model):
 
 class Message(models.Model):
     """
-
+    The model that describes Message object.
     """
-    sender = models.ForeignKey(User, null=False, related_name='user', on_delete=models.SET('User was delete'))
+    sender = models.ForeignKey(User, null=False, related_name='user_messages', on_delete=models.SET('User was delete'))
     text = models.TextField(null=True)
-    thread = models.ForeignKey(Thread, null=False, related_name='thread', on_delete=models.CASCADE)
+    thread = models.ForeignKey(Thread, null=False, related_name='thread_messages', on_delete=models.CASCADE)
     created = models.DateTimeField(editable=False, auto_now_add=True)
     is_read = models.BooleanField(null=False, default=False)
--- application/serializer.py
+++ application/serializer.py
@@ -4,7 +4,35 @@ from rest_framework import serializers
 from application import models
 
 
+class MessageForThreadSerializer(serializers.ModelSerializer):
+    """
+    Serializer that receives messages for a Thread in ThreadWithMessagesSerializer
+    """
+    sender = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='pk')
+
+    class Meta:
+        model = models.Message
+        fields = ('id', 'sender', 'text', 'created', 'is_read')
+
+
+class ThreadWithMessagesSerializer(serializers.ModelSerializer):
+    """
+    A serializer that outputs data about Thread and
+    all messages related with the Thread
+    """
+    created = serializers.DateTimeField(read_only=True)
+    thread_messages = MessageForThreadSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = models.Thread
+        fields = ('id', 'participants', 'created', 'updated', 'thread_messages')
+
+
 class ThreadSerializer(serializers.ModelSerializer):
+    """
+    A serializer that creates new Thread or
+    outputs data about Thread if it has been created.
+    """
     created = serializers.DateTimeField(read_only=True)
 
     def create(self, validated_data):
@@ -19,6 +47,9 @@ class ThreadSerializer(serializers.ModelSerializer):
 
 
 class MessageSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Message model
+    """
     sender = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='pk')
     thread = serializers.SlugRelatedField(queryset=models.Thread.objects.all(), slug_field='pk')
 
